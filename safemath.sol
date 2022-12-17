7c108f0d5915159443de8601f26a8853aabe0239
Kehinde - David Damilare Samuel <64425915+Qui-vira@users.noreply.github.com>
2022-12-17 12:59:28 +0100

    Create safemath.sol

---
+++ safemath.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract safemathtester {
+
+    uint8 public bigNumber = 255;
+
+    function add() public {
+      unchecked  {bigNumber = bigNumber + 1;}
+
+      /**
+        Enums
+        Events
+        Try/Catch
+        While Loop
+        Do-While
+        For
+        abi.encode/decode
+        hashing
+        Yul/Assembly
+       
+     **/
+
+    }
+}
